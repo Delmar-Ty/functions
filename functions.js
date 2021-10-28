a0da5f7bce6a3a60d792bec5c4a11bcb8d297a55
Delmar-Ty <89948545+Delmar-Ty@users.noreply.github.com>
2021-10-27 18:28:11 -0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,33 @@
+function circleArea(radius) {
+    var pi = 3.14;
+    console.log(radius);
+    var area = radius ** 2 * pi;
+    document.getElementById('area').innerHTML = area;
+}
+
+function compare(x, y) {
+    if (x > y) {
+        document.getElementById('compared').innerHTML = x + " Is Greater";
+    } else {
+        document.getElementById('compared').innerHTML = y + " Is Greater";
+    }
+}
+
+function EvenOdd(a) {
+    if (a % 2 === 0) {
+        document.getElementById('result').innerHTML = "Your number is even";
+    } else {
+        document.getElementById('result').innerHTML = "Your number is odd";
+    }
+}
+
+function palindrome(q) {
+    var array1 = q.split("");
+    var array2 = array1.reverse();
+    var final = array2.join("");
+    if (final == q) {
+        document.getElementById('hello').innerHTML = "Is a plaindrome";
+    } else {
+        document.getElementById('hello').innerHTML = "Not palindrome";
+    }
+}
